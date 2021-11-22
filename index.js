7f9530e3da190cce4c76e2e48a1bd171997ccea7
Joe McPartland <joemcpartland@gmail.com>
2021-11-22 10:02:59 -0700

    commit tree 3175ddc5f76cb9a0ec4daa4cdbf2bcc1fba8e3db

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let revWord = word.split('').reverse().join("");
+  if (word === revWord) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
